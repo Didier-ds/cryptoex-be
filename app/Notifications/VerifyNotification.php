3fd3d7dc5193303a268589ed5605b176b9eb5264
Ericogia <ogia.aghahowa@gmail.com>
2021-08-03 12:31:47 +0100

    Verify Notice Added

--- app/Notifications/VerifyNotification.php
+++ app/Notifications/VerifyNotification.php
@@ -11,47 +11,27 @@ class VerifyNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private $vetData;
+    public function __construct($vetData)
     {
-        //
+        $this->vetData = $vetData;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+
+            ->line($this->vetData['body'])
+            ->action($this->vetData['action'], $this->vetData['url'])
+            ->line($this->vetData['last']);
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
+
     public function toArray($notifiable)
     {
         return [
