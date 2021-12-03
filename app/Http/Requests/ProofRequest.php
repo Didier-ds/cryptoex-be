126909db7eb18f0d059efd16edcac24cd3aa3873
Ericogia <ogia.aghahowa@gmail.com>
2021-12-03 22:47:51 +0100

    Prrof Req

--- app/Http/Requests/ProofRequest.php
+++ app/Http/Requests/ProofRequest.php
@@ -13,7 +13,7 @@ class ProofRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class ProofRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'shot'   => 'required|image|mimes:jpeg,png,jpg,svg|max:2048',
+            'amount'    => 'required|string',
         ];
     }
 }
