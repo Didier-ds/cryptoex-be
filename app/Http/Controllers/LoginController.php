ad5ac542acf3847b7fd4a0a85cfa0d9024d850b4
Ericogia <ogia.aghahowa@gmail.com>
2021-12-03 10:30:49 +0100

    kjut

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Constants\Konstants;
 use App\Helpers\ResponseBuilder;
 use App\Http\Requests\LoginRequest;
+use App\Models\Konstants as ModelsKonstants;
 use App\Utils\Konstants as UtilsKonstants;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +14,7 @@ class LoginController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        return response()->json(UtilsKonstants::ERIC_API_URL);
+        return response()->json(ModelsKonstants::EMAIL);
         $credentials = $request->only(Konstants::EMAIL, Konstants::PWORD);
         if (!Auth::attempt($credentials)) {
             return  response(ResponseBuilder::genErrorRes(Konstants::ERR_INVALID_CRED), Konstants::STATUS_BAD_CRED);
---
+++ app/Models/Konstants.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Konstants
+{
+    // inputs keys
+    const FULLNAME = "fullname";
+    const EMAIL = "email";
+    const PWORD = "password";
+    const PHONE = "phone";
+    const A_TOK = 'auth-token';
+    const ACC_NO = "account_no";
+    const ACC_NAME = "account_name";
+    const BANK = "bank";
+
+    const DEFAULT = "none";
+
+    // Roles
+    const ROLE_OWNER = "owner";
+    const ROLE_ADMIN = "admin";
+    const ROLE_USER = "user";
+
+    const USER_ROLE_ADMIN = 1;
+    const USER_ROLE_USER = 2;
+
+    // Credentials Error MSG
+    const ERR_INVALID_CRED = "Invalid Email or Password";
+    const ERR_INVALID_EMAIL = "Email does not match any in our records";
+    const ERR_LACK_AUTH = "Lacking appropriate authorization";
+
+    // Request Status Code
+    const STATUS_ERROR = 500;
+    const STATUS_BAD_CRED = 406;
+    const STATUS_NOT_FOUND = 400;
+    const STATUS_OK = 200;
+    const STATUS_401 = 401;
+
+    const MSG_OK = "success";
+    const MSG_404 = "Resource not found";
+    const MSG_500 = "Unexpected Server Error";
+    const MSG_401 = "Forbidden";
+
+    const MESSAGE_CHECK_MAIL = "Please Check your email for details on how to proceed";
+    const MESSAGE_CEHECK_MAIL_LINK = "A verifiaction link has been sent to your email. Check and verify your account";
+    const MESSAGE_ADMIN_CHECK_MAIL = "Admin should check email and verify account";
+
+
+    // URLS
+    const URL_BASE = 'https://crypto-ex.netlify.app/';
+    const URL_VETTED = 'http://Crypto-ex.netlify.app/email/verification/when/now?verify=verified';
+}
