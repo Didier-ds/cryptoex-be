89173ef4879d5fed5e1b23bdbb28d4ae387fe82e
Ericogia <ogia.aghahowa@gmail.com>
2021-12-03 19:26:04 +0100

    Payment Scaffold

--- app/Models/PaymentProof.php
+++ app/Models/PaymentProof.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentProof extends Model
 {
     use HasFactory;
+    protected $table = 'payment_proof';
+    protected $fillable = ['uuid', 'image', 'amount', 'status'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
